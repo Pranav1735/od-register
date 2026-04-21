9c3a7e82ee588e40f45ec5277fcdfd5b8eb2b27b
Pranav R Raaj <140689235+Pranav1735@users.noreply.github.com>
2026-04-22 00:15:42 +0530

    Update SUPABASE_URL to remove endpoint path

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // =============================================
 // Get these from: https://supabase.com → Your Project → Settings → API
 
-const SUPABASE_URL = "https://yuzpsdchdvgejvenorfl.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://yuzpsdchdvgejvenorfl.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1enBzZGNoZHZnZWp2ZW5vcmZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3OTA2MTUsImV4cCI6MjA5MjM2NjYxNX0.divxcNd9ra5DSRgF3HgouNRNtYuDS3LQwqz3wCGAOyg";
 
 // =============================================
