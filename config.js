f245aa762a23056bd3855f44b5396961f5146be4
Pranav R Raaj <140689235+Pranav1735@users.noreply.github.com>
2026-04-22 00:53:35 +0530

    Fix duplicate SUPABASE_URL declaration
    Removed duplicate SUPABASE_URL declaration.

--- config.js
+++ config.js
@@ -3,11 +3,9 @@
 // =============================================
 // Get these from: https://supabase.com → Your Project → Settings → API
 
-<<<<<<< HEAD
+
 const SUPABASE_URL = "https://yuzpsdchdvgejvenorfl.supabase.co";
-=======
 const SUPABASE_URL = "https://yuzpsdchdvgejvenorfl.supabase.co  ";
->>>>>>> 45f0b42 (Updated project files)
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1enBzZGNoZHZnZWp2ZW5vcmZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3OTA2MTUsImV4cCI6MjA5MjM2NjYxNX0.divxcNd9ra5DSRgF3HgouNRNtYuDS3LQwqz3wCGAOyg";
 
 // =============================================
