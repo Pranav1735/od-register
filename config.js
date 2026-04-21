a3a1f20d92531da0b0fcbb5b0d36908a1e787654
Pranav R Raaj <140689235+Pranav1735@users.noreply.github.com>
2026-04-22 00:56:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,4 @@
-// =============================================
-//  STEP 1: PASTE YOUR SUPABASE DETAILS HERE
-// =============================================
-// Get these from: https://supabase.com → Your Project → Settings → API
-
-
 const SUPABASE_URL = "https://yuzpsdchdvgejvenorfl.supabase.co";
-const SUPABASE_URL = "https://yuzpsdchdvgejvenorfl.supabase.co  ";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1enBzZGNoZHZnZWp2ZW5vcmZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3OTA2MTUsImV4cCI6MjA5MjM2NjYxNX0.divxcNd9ra5DSRgF3HgouNRNtYuDS3LQwqz3wCGAOyg";
-
-// =============================================
-//  STEP 2: SET YOUR ADMIN PASSWORD
-// =============================================
-const ADMIN_PASSWORD = "Pranav.1735@";  // Change this to something secure!
-
-// =============================================
-//  STEP 3: (Optional) Set your college name
-// =============================================
-const COLLEGE_NAME = "National Service Scheme";
-
-
-
+const ADMIN_PASSWORD = "Pranav.1735@";
+const COLLEGE_NAME = "OD Register";
